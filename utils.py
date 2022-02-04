58e081ffe10e8632a1b93f247cbe8fc22c9de118
amunnelly <anthony.munnelly@gmail.com>
2022-02-04 22:45:38 +0000

    added a vowel checker for the permutations to cut down returns

--- utils.py
+++ utils.py
@@ -6,18 +6,19 @@ class Utilities(object):
     def get_permuations(word, pattern):
         if word == None:
             return ['shite', 'onions']
-        print(word)
-        print(pattern)
+        print(word, "!")
+        print(pattern, "!")
         word_count = len(pattern)
         pattern = compile_pattern(pattern)
-        print(pattern)
+        print(pattern, "!")
         letters = []
         [letters.append(i) for i in word]
         perms = []
         perms_beta = permutations(letters, word_count)
         for p in perms_beta:
             w = "".join(p)
-            if re.match(pattern, w):
+            vowels = vowel_checker(w)
+            if re.match(pattern, w) and vowels:
                 perms.append(w)
 
         perms = set(perms)
@@ -39,3 +40,11 @@ def compile_pattern(pattern_string):
     pattern = "".join(pattern)
 
     return re.compile(pattern)
+
+
+def vowel_checker(perm):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    for vowel in vowels:
+        if vowel in perm:
+            return True
+    return False
